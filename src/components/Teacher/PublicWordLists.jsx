808040379fd54609373cfcf4489939d9804e3e42
Brian Stasiukaitis <brianstas@gmail.com>
2025-11-10 23:43:33 +0900

    got it done
    moving right along

--- src/components/Teacher/PublicWordLists.jsx
+++ src/components/Teacher/PublicWordLists.jsx
@@ -47,6 +47,8 @@ const PublicWordLists = ({ onSelectWordList }) => {
     }
   };
 
+//making a comment of course
+
   const filteredLists = publicLists.filter(list => {
     if (searchTerm) {
       const searchLower = searchTerm.toLowerCase();
